e0d69e238960d9cc3ba4348cad07979944aa90cc
miriamR <105755R@gmail.com>
2026-04-23 17:23:59 +0300

    feat(api): initialize express application and server listener

---
+++ src/server.ts
@@ -0,0 +1,29 @@
+import app from './app.js';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+console.log("Starting server process..."); // שורת בדיקה
+
+
+
+const PORT = process.env.PORT||3002;
+const MONGODB_URI = process.env.MONGODB_URI
+
+const startServer = async () => {
+    try {
+        console.log("Attempting to connect to MongoDB...");
+        await mongoose.connect(MONGODB_URI as string);
+        console.log('✅ Connected to MongoDB');
+
+        app.listen(PORT, () => {
+            console.log(`🚀 Server is running on http://localhost:${PORT}`);
+        });
+    } catch (error) {
+        console.error('❌ Error during startup:', error);
+    }
+};
+
+// קריאה לפונקציה
+startServer();
