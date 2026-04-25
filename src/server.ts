dc97445d694b0643ac51f46f4ebf33407300ae0c
miriamR <105755R@gmail.com>
2026-04-25 22:49:25 +0300

    Fix port and add dockerfile for deployment

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ console.log("Starting server process..."); // שורת בדיקה
 
 
 
-const PORT = process.env.PORT||3002;
+const PORT = Number(process.env.PORT) || 3002;
 const MONGODB_URI = process.env.MONGODB_URI
 
 const startServer = async () => {
@@ -17,9 +17,14 @@ const startServer = async () => {
         await mongoose.connect(MONGODB_URI as string);
         console.log('✅ Connected to MongoDB');
 
-        app.listen(PORT, () => {
-            console.log(`🚀 Server is running on http://localhost:${PORT}`);
+        app.listen(PORT, "0.0.0.0", () => {
+            console.log(`🚀 Server is running on port ${PORT}`);
         });
+
+        // app.listen(PORT, () => {
+        //     console.log(`🚀 Server is running on http://localhost:${PORT}`);
+        // }
+        // );
     } catch (error) {
         console.error('❌ Error during startup:', error);
     }
